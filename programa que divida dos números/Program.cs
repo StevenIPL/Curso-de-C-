91537da1c704a93e3614a829c9a64117fa639d11
StevenIPL <guillenbritos@gmail.com>
2025-05-01 18:38:14 -0400

    no message

---
+++ programa que divida dos números/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el dividendo: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el divisor: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        if (num2 != 0)
+        {
+            double resultado = num1 / num2;
+            Console.WriteLine("Resultado: " + resultado);
+        }
+        else
+        {
+            Console.WriteLine("Error: No se puede dividir entre cero.");
+        }
+    }
+}
