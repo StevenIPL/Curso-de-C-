05107da270d99a80036e74bb2b6f96ca1ec00680
StevenIPL <guillenbritos@gmail.com>
2025-05-02 09:44:42 -0400

    no message

---
+++ Programa que multiplica dos números digitados/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int producto = num1 * num2;
+        Console.WriteLine("El resultado de la multiplicación es: " + producto);
+    }
+}
+
