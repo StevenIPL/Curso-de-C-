ae4983838599bfd1ef3dc43aae112258c8252924
StevenIPL <guillenbritos@gmail.com>
2025-05-03 10:07:03 -0400

    no message

---
+++ Programa que imprime los números impares del 1 al 30/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números impares del 1 al 30:");
+        for (int i = 1; i <= 30; i++)
+        {
+            if (i % 2 != 0)
+                Console.WriteLine(i);
+        }
+    }
+}
+
