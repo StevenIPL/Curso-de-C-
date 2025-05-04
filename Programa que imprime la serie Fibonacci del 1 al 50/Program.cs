0597df238c325c1e63c02c3650ccaea338908b3b
StevenIPL <guillenbritos@gmail.com>
2025-05-04 03:24:31 -0400

    no message

---
+++ Programa que imprime la serie Fibonacci del 1 al 50/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 0, b = 1, c = 0;
+
+        Console.WriteLine("Serie Fibonacci hasta 50:");
+        while (b <= 50)
+        {
+            Console.WriteLine(b);
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
+
