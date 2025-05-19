30e1f838b783b01280dc0f43f7ea35cfa6ec73d6
StevenIPL <guillenbritos@gmail.com>
2025-05-19 14:33:08 -0400

    no message

---
+++ Calculadora con menú en consola/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class Calculadora
+{
+    static void Main()
+    {
+        int opcion;
+        double num1, num2, resultado;
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("=== CALCULADORA ===\n");
+            Console.WriteLine("1 - Suma");
+            Console.WriteLine("2 - Resta");
+            Console.WriteLine("3 - Multiplicación");
+            Console.WriteLine("4 - División");
+            Console.WriteLine("5 - Elevar un número a la potencia");
+            Console.WriteLine("6 - Salir");
+            Console.Write("\nSeleccione una opción: ");
+
+            opcion = int.Parse(Console.ReadLine());
+
+            if (opcion >= 1 && opcion <= 5)
+            {
+                Console.Write("Ingrese el primer número: ");
+                num1 = double.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el segundo número: ");
+                num2 = double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                num1 = num2 = resultado = 0;
+            }
+
+            switch (opcion)
+            {
+                case 1:
+                    resultado = num1 + num2;
+                    Console.WriteLine($"\nResultado: {num1} + {num2} = {resultado}");
+                    break;
+                case 2:
+                    resultado = num1 - num2;
+                    Console.WriteLine($"\nResultado: {num1} - {num2} = {resultado}");
+                    break;
+                case 3:
+                    resultado = num1 * num2;
+                    Console.WriteLine($"\nResultado: {num1} * {num2} = {resultado}");
+                    break;
+                case 4:
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                        Console.WriteLine($"\nResultado: {num1} / {num2} = {resultado}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nError: División por cero.");
+                    }
+                    break;
+                case 5:
+                    resultado = Math.Pow(num1, num2);
+                    Console.WriteLine($"\nResultado: {num1} ^ {num2} = {resultado}");
+                    break;
+                case 6:
+                    Console.WriteLine("\nSaliendo del programa...");
+                    break;
+                default:
+                    Console.WriteLine("\nOpción inválida. Intente de nuevo.");
+                    break;
+            }
+
+            if (opcion != 6)
+            {
+                Console.WriteLine("\nPresione cualquier tecla para continuar...");
+                Console.ReadKey();
+            }
+
+        } while (opcion != 6);
+    }
+}
---
+++ Calculadora con menú en consola/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Calculadora con menú en consola")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculadora con menú en consola")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("d3e980f1-e9c4-435a-8a0a-5d29c21db4a0")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
